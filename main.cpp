6bf9592b1ea5d4ce61707f72e7632ac67f289c13
Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
2021-12-11 23:42:42 +0100

    feature(Main menu): Added main menu

--- main.cpp
+++ main.cpp
@@ -20,25 +20,28 @@ int main() {
 			int currentYear = aTime->tm_year + 1900;
 			cout << "Hi, I am " << botName + ", " + developer + "'s bot, to see the commands list, type:" << endl;
 			cout << "!cmd" << endl;
-			cout << developer + " " + emoji::copyright + " 2016-" << currentYear << stringUtils::endLines(2);
+			cout << developer + " " + emoji::copyright + " 2016-" << currentYear << stringUtils::emptyLines(2);
 			start = true;
 		}
 		cout << "Insert your command: ";
 		std::string command;
 		cin >> command;
 		if(botCommand(command, botName))
-			cout << "Tell me my love " + emoji::heart << stringUtils::endLines(2);
+			cout << "Tell me my love " + emoji::heart << stringUtils::emptyLines(2);
 		else if(botCommand(command, "clear")) {
 			system("clear");
-			cout << "Console cleaned!" << stringUtils::endLines(2);
-		}
-		else if(botCommand(command, "quit") || botCommand(command, "exit")) {
+			cout << "Console cleaned!" << stringUtils::emptyLines(2);
+		} else if(botCommand(command, "quit") || botCommand(command, "exit")) {
 			string responses[] = {"At the next time", "See you soon", "Farewell"};
-			string response = responses[rand() % size(responses)];
-			cout << response << stringUtils::endLines(2);
+			cout << responses[rand() % size(responses)] << endl;
 			exit(0);
-		}
-		else
-			cout << "This is not my command" << stringUtils::endLines(2);
+		} else if(botCommand(command, "!cmd")) {
+			cout << "Here bot mode available:" + stringUtils::emptyLines(2);
+			cout << emoji::robot + ") €" + stringUtils::lowercase(botName) + "Bot - " + botName + " classic commands";
+			cout << stringUtils::emptyLines(2);
+			cout << emoji::robot + ") €teenyBot - TeenyBot mode commands" << stringUtils::emptyLines(2);
+			cout << emoji::sosSignal + ") /covid - Check news about coronavirus in italy";
+		} else
+			cout << "This is not my command" << stringUtils::emptyLines(2);
 	}
 }
