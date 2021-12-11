93e5bccce1f8ce2ee6ad3029c7b9ddd4379c7626
Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
2021-12-11 22:25:46 +0100

    feature(StringUtils): Added emoji class

---
+++ emoji.h
@@ -0,0 +1,15 @@
+//
+// Created by goddino on 12/11/21.
+//
+#include <string>
+#ifndef CPPALESSIABOT_EMOJI_H
+#define CPPALESSIABOT_EMOJI_H
+
+
+class emoji {
+public:
+	[[maybe_unused]] static std::string heart;
+};
+
+
+#endif //CPPALESSIABOT_EMOJI_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
+#include "stringUtils.h"
+#include "emoji.h"
+#include <ctime>
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+	srand(time(0));
+	while(true) {
+		cout << "Insert your command: ";
+		std::string command;
+		cin >> command;
+		if(stringUtils::lowercase(command) == "alessia")
+			cout << "Tell me my love " << emoji::heart << endl;
+		else if(stringUtils::lowercase(command) == "quit") {
+			string responses[] = {"At the next time", "See you soon", "Farewell"};
+			string response = responses[rand() % size(responses)];
+			cout << response;
+			exit(0);
+		}
+		else
+			cout << "This is not my command";
+	}
 }
