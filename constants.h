fcb0b2951aaccda4d2f4ff374de112557605e000
Lorenzo Pellegrini <lorenzo.pellegrini@intramind-srl.com>
2021-12-11 23:17:40 +0100

    feature(Constants): Added constants header file

---
+++ constants.h
@@ -0,0 +1,11 @@
+//
+// Created by goddino on 12/11/21.
+//
+
+#ifndef CPPALESSIABOT_CONSTANTS_H
+#define CPPALESSIABOT_CONSTANTS_H
+
+const string developer = "Goddino";
+const string botName = "Alessia";
+
+#endif //CPPALESSIABOT_CONSTANTS_H
